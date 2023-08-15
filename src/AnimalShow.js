818a7758d944c936c58c1491a1d75ce7761770f6
Haroun Ted <harounted@gmail.com>
2023-08-15 21:45:07 +0300

    add randome function

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,7 @@
 
 
-function AnimalShow(){
-
+function AnimalShow({type}){
+    return <div>{type}</div>
 }
 
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import AnimalShow from "./AnimalShow";
+import { useState } from "react";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "dog", "cow", "aligator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-    
+  const [animals, setAnimals] = useState([]);
 
-  return (<div>
-    <button onClick={()=> console.log("the button was clicked")}>Add Animal</button>
-  </div>);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>
+        <AnimalShow />
+      </div>
+    </div>
+  );
 }
 export default App;
