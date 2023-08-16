69c77c4c884f250db0b582c13dc54e65b0aafdbc
Haroun Ted <harounted@gmail.com>
2023-08-16 12:55:09 +0300

    add svgs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import AnimalShow from "./AnimalShow";
 import { useState } from "react";
+import "./App.css";
 
 function getRandomAnimal() {
-  const animals = ["bird", "cat", "dog", "cow", "aligator", "horse"];
+  const animals = ["bird", "cat", "dog", "cow", "gator", "horse"];
 
   return animals[Math.floor(Math.random() * animals.length)];
 }
 
 function App() {
   const [animals, setAnimals] = useState([]);
-
   const handleClick = () => {
-    setAnimals([...animals, getRandomAnimal()]);
+    if (animals.length < 6) {
+      const newAnimal = getRandomAnimal();
+      if (!animals.includes(newAnimal)) {
+        setAnimals([...animals, newAnimal]);
+      } else {
+        handleClick();
+      }
+    } else {
+      console.log("no more animals for you bitch !!");
+    }
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>
-        <AnimalShow />
-      </div>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
